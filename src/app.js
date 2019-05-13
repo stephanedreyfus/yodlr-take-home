1d4501fe0dceb3a9f09fc4c210bff247018a77d0
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-12 20:48:00 -0700

    Install React, create App, and update index.html.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+        <h1>Yodlr Design Challenge</h1>
+        <p>
+          <a href="/signup.html">Registration Page</a><br/>
+          <a href="/admin.html">Admin Page</a>
+        </p>
+      </body>
+    )
+  }
+}
+
+export default App;
